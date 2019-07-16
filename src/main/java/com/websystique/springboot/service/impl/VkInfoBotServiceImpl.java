4a5755dbf0b255a769c7b6b6bc8cce4cf360cbdd
Tyurin Denis <dent.ttt@gmail.com>
2019-07-17 01:13:15 +0400

    Commit #13. - Изменен SQL запрос для оптимизации вывода информации о клиенте. - Удален лишний код, который стал лишним из-за оптимизации запроса к ДБ

--- src/main/java/com/websystique/springboot/service/impl/VkInfoBotServiceImpl.java
+++ src/main/java/com/websystique/springboot/service/impl/VkInfoBotServiceImpl.java
@@ -17,29 +17,21 @@ import java.util.*;
 @Component
 public class VkInfoBotServiceImpl implements VkInfoBotService {
     private static Logger logger = LoggerFactory.getLogger(VkInfoBotServiceImpl.class.getName());
-    private final String SQL_QUERY = "SELECT \n" + // TODO: 12.07.2019 Добавить упорядочивание по фамилии
-            "    cl.client_id,\n" +
-            "    cl.last_name,\n" +
-            "    cl.first_name,\n" +
-            "    cl.email,\n" +
-            "    cl.phone_number,\n" +
-            "    cl.city,\n" +
-            "    cl.country,\n" +
-            "    cl.client_description_comment,\n" +
-            "    cl.comment,\n" +
-            "    cl.client_state,\n" +
-            "    cl.postpone_comment,\n" +
-            "    cl.birth_date,\n" +
-            "    u.last_name AS Owner_lastName,\n" +
-            "    u.first_name AS Owner_name,\n" +
-            "    u1.last_name AS Mentor_lastName,\n" +
-            "    u1.first_name AS Mentor_name\n" +
-            "FROM\n" +
-            "    CLIENT cl\n" +
-            "        LEFT JOIN\n" +
-            "    user u ON cl.owner_user_id = u.user_id\n" +
-            "        LEFT JOIN\n" +
-            "    user u1 ON cl.owner_mentor_id = u1.user_id\n" +
+    private final String SQL_QUERY = "SELECT\n" +
+            "cl.client_id AS id,\n" +
+            "concat(cl.last_name, ' ', cl.first_name) AS fio,\n" +
+            "concat('Email: ', cl.email, ', телефон: ', cl.phone_number) AS contacts," +
+            "concat(cl.country, ',', ' ', cl.city) AS lives_in,\n" +
+            "cl.client_description_comment AS description,\n" +
+            "cl.comment,\n" +
+            "cl.postpone_comment AS p_comment,\n" +
+            "concat(u.last_name, ' ', u.first_name) AS owner,\n" +
+            "concat(u1.last_name, ' ', u1.first_name) AS mentor\n" +
+            "FROM CLIENT cl\n" +
+            "LEFT JOIN\n" +
+            "USER u ON cl.owner_user_id = u.user_id\n" +
+            "LEFT JOIN\n" +
+            "user u1 ON cl.owner_mentor_id = u1.user_id\n" +
             "WHERE ";
     private final String VK_URL_API;
     private final String VK_API_VERSION;
@@ -56,10 +48,10 @@ public class VkInfoBotServiceImpl implements VkInfoBotService {
     public VkInfoBotServiceImpl(VkInfoBotConfig vkInfoBotConfig,
                                 @Qualifier("TestClientCustomRepositoryImpl") TestClientCustomRepository clientCustomRepository) {
         Command findById = new Command("^i$|^и$|^ид$|^id$", 1,
-                new String[]{"^[0-9]+\\*?$|^\\*[0-9]+$"}, SQL_QUERY + "CL.CLIENT_ID RLIKE('%s') LIMIT %s");
+                new String[]{"^[0-9]+\\*?$|^\\*[0-9]+$"}, SQL_QUERY + "cl.client_id RLIKE('%s') LIMIT %s");
         Command findByCityAndLastName = new Command("^cl$|^cln$|^сл$|^слн$", 2,
                 new String[]{"^[a-zа-я]+\\*?$|^\\*[a-zа-я]+$", "^[a-zа-я]+\\*?$|^\\*[a-zа-я]+$"},
-                SQL_QUERY + "CL.CITY RLIKE('%s') AND CL.LAST_NAME RLIKE('%s') LIMIT %s");
+                SQL_QUERY + "cl.city RLIKE('%s') AND cl.last_name RLIKE('%s') LIMIT %s");
         commandSet = new HashSet<>(Arrays.asList(findById, findByCityAndLastName));
 
         this.clientCustomRepository = clientCustomRepository;
@@ -136,25 +128,18 @@ public class VkInfoBotServiceImpl implements VkInfoBotService {
     }
 
     private List<String> formTextViewOfClientList(List<Object[]> clients) {
-        String[] prefixes = new String[]{"ID", "Фамилия", "Имя", "Email", "Телефон", "Город", "Страна", "Описание", "Комментарий",
-                "Состояние", "Отложенный комментарий", "Дата рождения", "Менеджер", "Ментор"};
+        String[] prefixes = new String[]{"ID:", "ФИО:", "", "Живет в:", "Описание:", "Комментарий:",
+                "Отложенный комментарий:", "Менеджер:", "Ментор:"};
         List<String> clientsList = new ArrayList<>();
         for (Object[] objects : clients) {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("************************** CLIENT **************************\n");
-            for (int i = 0; i < 12; i++) {
+            // TODO: 17.07.2019 подумать как красиво вывести первую строку
+//            stringBuilder.append("************************** CLIENT **************************\n");
+            for (int i = 0; i < prefixes.length; i++) {
                 if (objects[i] != null) {
-                    stringBuilder.append(" ").append(prefixes[i]).append(": ").append(objects[i]).append("\n");
+                    stringBuilder.append("> ").append(prefixes[i]).append(" ").append(objects[i]).append("\n");
                 } else {
-                    stringBuilder.append(" ").append(prefixes[i]).append(": ").append("Не указано").append("\n");
-                }
-            }
-            for (int i = 12, j = 12; i < 16; i += 2, j++) {
-                if (objects[i] != null) {
-                    stringBuilder.append(" ").append(prefixes[j]).append(": ").append(objects[i]).append(" ").
-                            append(objects[i + 1]).append("\n");
-                } else {
-                    stringBuilder.append(" ").append(prefixes[j]).append(": ").append("Не указано").append("\n");
+                    stringBuilder.append("> ").append(prefixes[i]).append(" ").append("N/A").append("\n");
                 }
             }
             clientsList.add(stringBuilder.toString());
